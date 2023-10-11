11407ac98edbab1a75089d3c52e25a56b03e9bc5
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-11 01:01:21 -0400

    ci debug used chai.request

--- server/integration-test/auth.tets.js
+++ server/integration-test/auth.tets.js
@@ -11,7 +11,7 @@ describe("Integration testing", () => {
   it("should connect to DB", (done) => {
     chai
     .request(app)
-    .get("/healthz")
+    .get("/health")
     .end((error, res) => {
       if (error) done(err);
       chai.expect(res.statusCode).to.equal(200);
