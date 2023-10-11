345160c9598843a01c50669579606acbb3207c94
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-11 00:38:02 -0400

    ci debug used chai.request

--- deployment/start.sh
+++ deployment/start.sh
@@ -4,5 +4,6 @@ sudo apt install nodejs npm -y
 
 sudo service postgresql start
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+sudo -u postgres psql -c "CREATE DATABASE csye6225;"
 
 sudo chmod 747 /opt
