6574c2af3c5cb16cbd91f8b21f501d46d2a76837
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-11 00:59:00 -0400

    ci debug used chai.request

--- server/integration-test/auth.tets.js
+++ server/integration-test/auth.tets.js
@@ -8,32 +8,14 @@ const app = require("../index");
 chai.use(chaiHttp);
 
 describe("Integration testing", () => {
-  before("Initalize application", function (done) {
-    this.timeout(4000);
-    setTimeout(() => {
-      done();
-    }, 3000);
-  });
-
   it("should connect to DB", (done) => {
     chai
     .request(app)
-    .get("/health")
+    .get("/healthz")
     .end((error, res) => {
       if (error) done(err);
       chai.expect(res.statusCode).to.equal(200);
       done();
     })
-  });
-
-  after("close open connection", function (done) {
-    this.timeout(5000);
-    setTimeout(() => {
-      Connection.closeConnection().then((isDBClosed) => {
-        chai.assert.isTrue(isDBClosed);
-        done();
-        process.exit(0);
-      });
-    }, 3000);
   });
 });
