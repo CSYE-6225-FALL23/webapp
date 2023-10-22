32d652077e77c34f47d4dab7ed2e9aec71d5954f
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-22 01:28:25 -0400

    removed postgres and changed webapp path

--- deployment/ami/ami_init.sh
+++ deployment/ami/ami_init.sh
@@ -7,15 +7,8 @@ sudo apt upgrade -y
 sudo apt-get -y remove git
 sudo apt-get -y autoremove
 
-# Access to copy user.csv
-sudo chmod 747 /opt
-
 # Install PostgreSQL
 sudo apt install unzip postgresql postgresql-contrib -y
-
-# Start and enable PostgreSQL service
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
 
 # Install Node.js and npm
 sudo apt install nodejs npm -y
@@ -23,10 +16,3 @@ sudo apt install nodejs npm -y
 # Verify Node.js and npm installation
 node -v
 npm -v
-
-# Create a PostgreSQL user and database
-sudo -u postgres psql -c "CREATE DATABASE ${POSTGRES_DB} OWNER postgres;"
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD ${POSTGRES_PASSWORD};"
-
-# Restart PostgreSQL to apply the changes
-sudo systemctl restart postgresql
