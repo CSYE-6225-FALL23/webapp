fc6db0f7e02c09cc333571fad5671f88d1cf1994
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-25 19:22:49 -0400

    only user creating in ami

--- deployment/ami/ami_init.sh
+++ deployment/ami/ami_init.sh
@@ -20,14 +20,6 @@ npm -v
 APP_USER=csye6225
 APP_USER_PASSWORD=csye6225
 APP_GROUP=csye6225
-APP_DIR="/var/www/webapp"
-
-# Create the destination directory and copy files
-sudo mkdir -p /var/www/
-sudo cp -rf /home/admin/webapp.zip /var/www/
-
-cd /var/www
-sudo unzip -o webapp.zip -d $APP_DIR
 
 # Create the user
 sudo useradd -m $APP_USER
@@ -38,18 +30,3 @@ echo "$APP_USER:$APP_USER_PASSWORD" | sudo chpasswd
 
 # Add the user to the group
 sudo usermod -aG $APP_GROUP $APP_USER
-
-# Set directory permissions
-sudo chown -R $APP_USER:$APP_GROUP $APP_DIR
-sudo find $APP_DIR -type d -exec chmod 750 {} \\;
-sudo find $APP_DIR -type f -exec chmod 640 {} \\;
-sudo chmod 650 $APP_DIR/server/index.js
-
-# Systemd file
-sudo cp $APP_DIR/deployment/webapp.service /lib/systemd/system
-sudo chown $APP_USER:$APP_GROUP /lib/systemd/system/webapp.service
-sudo chmod 550 /lib/systemd/system/webapp.service
-
-systemctl daemon-reload
-sudo systemctl enable webapp.service
-sudo systemctl start webapp.service
