d7b1dc4c87dddd37706ce53201a3729d36fb6b68
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-25 19:08:37 -0400

    user creation login in ami

--- deployment/ami/ami_init.sh
+++ deployment/ami/ami_init.sh
@@ -16,3 +16,31 @@ sudo apt install unzip nodejs npm -y
 # Verify Node.js and npm installation
 node -v
 npm -v
+
+APP_USER=csye6225
+APP_USER_PASSWORD=csye6225
+APP_GROUP=csye6225
+APP_DIR="/var/www/webapp"
+
+# Create the destination directory and copy files
+sudo mkdir -p /var/www/
+sudo cp -rf /home/admin/webapp.zip /var/www/
+
+cd /var/www
+sudo unzip -o webapp.zip -d $APP_DIR
+
+# Create the user
+sudo useradd -m $APP_USER
+sudo groupadd $APP_GROUP
+
+# Change user password
+echo "$APP_USER:$APP_USER_PASSWORD" | sudo chpasswd
+
+# Add the user to the group
+sudo usermod -aG $APP_GROUP $APP_USER
+
+# Set directory permissions
+sudo chown -R $APP_USER:$APP_GROUP $APP_DIR
+sudo find $APP_DIR -type d -exec chmod 750 {} \\;
+sudo find $APP_DIR -type f -exec chmod 640 {} \\;
+sudo chmod 650 $APP_DIR/server/index.js
