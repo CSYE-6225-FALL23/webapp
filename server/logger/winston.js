be0a643b8b2bb3df5a79637cff48a8f70449a8c1
dev-kudli <sudarshan97.kudli@gmail.com>
2023-11-05 19:33:37 -0500

    edited logs

--- server/logger/winston.js
+++ server/logger/winston.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.File({ filename: '../webapp.log' }),
+    new winston.transports.File({ filename: '/var/log/webapp.log' }),
   ],
 });
 
