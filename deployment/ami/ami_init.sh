024796fdd41d34886ddd55c3017711ffd4766e4f
dev-kudli <sudarshan97.kudli@gmail.com>
2023-11-06 00:19:27 -0500

    removed env file from init script

--- deployment/ami/ami_init.sh
+++ deployment/ami/ami_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+APP_USER=csye6225
+APP_USER_PASSWORD=csye6225
+APP_GROUP=csye6225
+APP_DIR="/var/www/webapp"
+
 # Update the package list and upgrade the system
 sudo apt update
 sudo apt upgrade -y
@@ -14,26 +19,16 @@ sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 # Install Node.js and npm
 sudo apt install unzip nodejs npm -y
 
-APP_DIR="/var/www/webapp"
-
 # Create the destination directory and copy files
 sudo mkdir -p /var/www/
 sudo cp -rf /home/admin/webapp.zip /var/www/
 sudo unzip -o /var/www/webapp.zip -d $APP_DIR
-
-# ENV file
-sudo touch /opt/.env.prod
 
 # Copy systemd file
 sudo cp $APP_DIR/deployment/webapp.service /lib/systemd/system
 
 # Copy config and start cloudwatch agent
 sudo cp -f $APP_DIR/deployment/config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
-
-APP_USER=csye6225
-APP_USER_PASSWORD=csye6225
-APP_GROUP=csye6225
-ENV_DIR="/opt/.env.prod"
 
 # Create the user
 sudo useradd -m $APP_USER
