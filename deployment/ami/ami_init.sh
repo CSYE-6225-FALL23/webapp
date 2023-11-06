35adc0a2a522ea11fefdf91b3274ff80fe131ea1
dev-kudli <sudarshan97.kudli@gmail.com>
2023-11-05 22:41:04 -0500

    systemd in ami

--- deployment/ami/ami_init.sh
+++ deployment/ami/ami_init.sh
@@ -29,3 +29,38 @@ sudo cp $APP_DIR/deployment/webapp.service /lib/systemd/system
 
 # Copy config and start cloudwatch agent
 sudo cp -f $APP_DIR/deployment/config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
+
+APP_USER=csye6225
+APP_USER_PASSWORD=csye6225
+APP_GROUP=csye6225
+ENV_DIR="/opt/.env.prod"
+
+# Create the user
+sudo useradd -m $APP_USER
+sudo groupadd $APP_GROUP
+
+# Change user password
+echo "$APP_USER:$APP_USER_PASSWORD" | sudo chpasswd
+
+# Add the user to the group
+sudo usermod -aG $APP_GROUP $APP_USER
+
+# Change directory owner and permissions
+sudo chown -R $APP_USER:$APP_GROUP $APP_DIR
+sudo find $APP_DIR -type d -exec chmod 750 {} \\;
+sudo find $APP_DIR -type f -exec chmod 640 {} \\;
+sudo chmod 650 $APP_DIR/server/index.js
+
+# Permission for systemd file
+sudo chown $APP_USER:$APP_GROUP /lib/systemd/system/webapp.service
+sudo chmod 550 /lib/systemd/system/webapp.service
+
+# Permission for log file
+sudo touch /var/log/webapp.log
+sudo chown $APP_USER:$APP_GROUP /var/log/webapp.log
+sudo chmod 660 /var/log/webapp.log
+
+# Start systemd service
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
