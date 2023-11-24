760229a4f4d7e890469e8c870b9fceba2e12f852
dev-kudli <sudarshan97.kudli@gmail.com>
2023-11-24 10:43:33 -0500

    push message to sns

---
+++ server/helper/sns.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+
+const region = process.env.SNS_REGION;
+const topicArn = process.env.SNS_TOPIC_ARN;
+
+// Create an SNS client
+const sns = new AWS.SNS({ region });
+
+// Publish the message to the SNS topic
+exports.publishSubmissionMessage = async (message) => {
+    try {
+        const data = await sns.publish({ Message: JSON.stringify(message), TopicArn: topicArn }).promise();
+        return { status: 'success', messageId: data.MessageId };
+    } catch (err) {
+        throw new Error("Failed to public topic");
+    }
+};
